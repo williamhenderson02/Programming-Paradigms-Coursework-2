2c0e5ad57870409f36cf7df2de5a808c09d16afc
William Henderson <tgpz35@durham.ac.uk>
2023-01-06 16:40:44 +0000

    added basic turning and moving

--- langton.c
+++ langton.c
@@ -5,7 +5,7 @@
 
 int main(){
     //printf("x = %d y = %d direction = %d", ant1.x, ant1.y, ant1.direction);
-    struct ant ant1 = {1,2,UP};
+    struct ant ant1 = {10,15,RIGHT};
     turn_left(&ant1);
     turn_right(&ant1);
     move_forward(&ant1);
@@ -13,15 +13,50 @@ int main(){
 }
 
 void turn_left(struct ant *ant){
-    printf("x = %d y = %d direction = %d\n", ant->x, ant->y, ant->direction);
+    printf("direction = %d\n",ant->direction);
+    if(ant->direction  == 0)
+        ant->direction = 2;
+    else if(ant->direction  == 1)
+        ant->direction = 3;
+    else if(ant->direction  == 2)
+        ant->direction = 1;
+    else
+        ant->direction = 0;
+    printf("direction = %d\n", ant->direction);
 }
 
 void turn_right(struct ant *ant){
-    printf("x = %d y = %d direction = %d\n", ant->x, ant->y, ant->direction);
+    printf("direction = %d\n",ant->direction);
+    if(ant->direction  == 0){
+        ant->direction = 3;
+    }
+    else if(ant->direction  == 1){
+        ant->direction = 2;
+    }
+    else if(ant->direction  == 2){
+        ant->direction = 0;
+    }
+    else {
+        ant->direction = 1;
+    }
+    printf("direction = %d\n", ant->direction);
 }
 
 void move_forward(struct ant *ant){
-    printf("x = %d y = %d direction = %d", ant->x, ant->y, ant->direction);
+    printf("x = %d y = %d\n", ant->x, ant->y);
+    if(ant->direction == 0){
+        ant->y += 1;
+    }
+    else if(ant->direction == 1){
+        ant->y -= 1;
+    }
+    else if(ant->direction == 2){
+        ant->x -= 1;
+    }
+    else {
+        ant->x += 1;
+    }
+    printf("x = %d y = %d\n", ant->x, ant->y);
 }
 
 /*
