69eec819cd29f8bdd9dad542a0bfe56bf32f4f55
William Henderson <tgpz35@durham.ac.uk>
2023-01-29 23:13:47 +0000

    main linted

--- main.c
+++ main.c
@@ -13,54 +13,50 @@ enum colour colour;
 enum direction d;
 struct rule rule = {};
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     global_argc = argc;
     global_argv = argv;
     int i;
     char temp;
 
-    if(global_argc < 2){
+    if (global_argc < 2) {
     start_visualisation(&ant);
-    do{
-        for(i = 0; i < 100; i++){
+    do {
+        for (i = 0; i < 100; i++) {
         visualise_and_advance(&ant);
         }
     }
-    while(not_quit());  
+    while (not_quit());
 
     end_visualisation();
-    }
-    else if(global_argc > 2){
-        printf("Too many arguments given. The program should be invoked as './ant {rule}");
-    }
-    else{
+    } else if (global_argc > 2) {
+        printf("Too many arguments given."
+                " The program should be invoked as './ant {rule}");
+    } else {
         char *input = global_argv[1];
-        for(int i = 0; i < strlen(input); i++){
-            if(input[i]!= 'l' && input[i] != 'L' && input[i] != 'r' && input[i] != 'R'){
-            printf("{rule} must only include 'L' and 'R'");
-            return 0;
-            }
-            else{
+        for (int i = 0; i < strlen(input); i++) {
+            if (input[i]!= 'l' && input[i] != 'L' && input[i] != 'r' &&
+            input[i] != 'R') {
+                printf("{rule} must only include 'L' and 'R'");
+                return 0;
+            } else {
                 while (input[i]) {
-                temp = input[i];
-                input[i] = toupper(temp);
-                i++;
-    }
+                    temp = input[i];
+                    input[i] = toupper(temp);
+                    i++;
+                }
             }
-
         }
-        //rule.states = strlen(input);
         rule.rules = input;
-        
-        /*start_visualisation(&ant);
-    do{
-        for(i = 0; i < 100; i++){
-        visualise_and_advance(&ant);
+        start_visualisation(&ant);
+    do {
+        for (i = 0; i < 100; i++) {
+            visualise_and_advance(&ant);
         }
     }
-    while(not_quit());  
+    while (not_quit());
 
-    end_visualisation();*/
+    end_visualisation();
     }
     return 0;
 }
